742d2e7f9a05b66932d3524fdef1102ca5be5953
aboanas <aboanas.tester@gmail.com>
2018-09-27 16:37:51 +0200

    protected access modefier

--- Student.php
+++ Student.php
@@ -13,17 +13,128 @@
  */
 class Student {
     //put your code here
-    public $name;
-    public $age;
-    public $level;
-    public $score;
-    public $subjects = array(
+    protected $name;
+    protected $age;
+    protected $level;
+    protected $score;
+    protected $subjects = array(
         'Arabic'  => 0,
         'English' => 0,
         'Math'    => 0,
         'Science' => 0,
     );
     
-    public $minscore = 150;
-    public $maxscore = 300;
+    protected $minScore = 150;
+    protected $maxScore = 300;
+    
+    public function setName($name){
+        $this->name = filter_var(ucwords(substr($name, 0)), FILTER_SANITIZE_STRING);
+     }
+     
+    public function getName(){
+        return $this->name;
+    }
+
+    public function setLevel($level){
+        $level = filter_var($level, FILTER_SANITIZE_NUMBER_INT);
+        $level = abs($level);
+        if($level < 1 || $level > 6){
+            throw new Exception('Sorry');
+        } else {
+            $this->level = $level;
+        }
+    }
+    
+    public function getLevel(){
+        return $this->level;
+    }
+    
+    public function setAge($age){
+        $age = filter_var($age, FILTER_SANITIZE_NUMBER_INT);
+        if($age < 12){
+            throw new Exception("Sorry , you are too young");
+        } else{
+            return $this->age = $age;
+        }
+    }
+    
+    public function getAge(){
+        return $this->age;
+    }
+    
+    public function getMinScore(){
+        return $this->minScore;
+    }
+    
+    public function setMinScore($minScore){
+        
+    }
+    public function __construct($name, $age) {
+        $this->setName($name);
+        
+        if($age > 12){
+           $this->setAge($age);
+        } else {
+            throw new Exception("Sorry the student age should be greater than 12");
+        }
+       
+    }
+    
+    public function setSubjects($subjectName, $score){
+        
+        if(array_key_exists($subjectName, $this->subjects)){
+            $score = filter_var($score, FILTER_SANITIZE_NUMBER_INT);
+            $this->subjects[$subjectName] = $score;
+        } else {
+            throw new Exception("This subject is not exist");
+        }
+    }
+    
+    public function  getSubjects(){
+        return $this->subjects;
+    }
+    
 }
+
+class StudentGrade1 extends Student{
+    
+    public function __construct($name, $age) {
+        
+        parent:: __construct($name, $age);
+        
+        $this->minScore = 300;
+        $this->maxScore = 500;
+        $this->subjects['computers'] = 0;
+       
+    }
+
+
+    // Custom constructor for OptimalStudent will invoked & the parent will not invoked absulotely
+   
+}
+
+$stdg1 = new StudentGrade1("abaas adel mustafa easa", 15);
+echo '<pre>';
+    var_dump($stdg1);
+echo '</pre>';
+
+$stdg1->setSubjects('Arabic', 30);
+
+print_r($stdg1->getSubjects());
+
+// Once new obj created, the constructor of this class will invoked
+
+//$std1 = new Student('ali', 16);
+//
+//// Set the name property
+//$std1->setName('mohammed mohammed abosalim');
+//// Get the name property
+//echo "First student Name: " . $std1->getName() . '<br>';
+//
+//$std1->setAge(25);
+//echo "First student Age: " . $std1->getAge() . '<br>';
+//
+//$std1->setLevel(4);        
+//echo "First student Level: " . $std1->getLevel() . '<br>';
+//
+//echo $std1->getMinScore();
